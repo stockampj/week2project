f901e65ea2aa8c2a29e6200741de10da8a2933f6
Joel Stockamp <stockampj@gmail.com>
2019-08-16 11:34:51 -0700

    define variable in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function(event) {
   $(".submit").click(function() {
 
+    var ruby = 0;
+    var csharp = 0;
+    var javascript = 0;
+    var go = 0;
+    var python = 0;
+    var swift = 0;
+
+    // Animal question
+    var q1val = $(#q1-ui).val();
+    // Color question
+    var q2val = $(#q2-ui).val();
+
+    var q3val = $(#q3-ui).val();
+    var q4val = $(#q4-ui).val();
+    var q5val = $(#q5-ui).val();
 
 
     event.preventDefault();
