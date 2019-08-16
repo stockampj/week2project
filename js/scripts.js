bc0cae663376a07de926901eacdfaa64e5cf4f9d
Joel Stockamp <stockampj@gmail.com>
2019-08-16 13:04:49 -0700

    add a bit of styling and js logic to responses

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,9 @@ $(document).ready(function(event) {
     var q2val = $(#q2-ui).val();
     // Shape question
     var q3val = $(#q3-ui).val();
-
+    // Name length Question
     var q4val = $(#q4-ui).val();
+
     var q5val = $(#q5-ui).val();
 
 
