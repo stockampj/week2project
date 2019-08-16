f59fa3b89d5be9a74da2cfbe26ce90e2d7d80cf2
Joel Stockamp <stockampj@gmail.com>
2019-08-16 09:19:26 -0700

    add basic divisions and objects to index.html

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(event) {
+  $(".submit").click(function() {
+
+
+
+    event.preventDefault();
+  });
+});
+
+
+
+// var weapon = $("#q1").val();
+// var terrain = $("#q2").val();
+// var style = $("#q3").val();
+//
+// $("#q1r").text(weapon);
+// $("#q2r").text(terrain);
+// $("#q3r").text(style);
+// $("#concat").text(weapon+terrain+style);
