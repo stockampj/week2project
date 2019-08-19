2320d1fbdf4fdfbafe50ccfa9a060f1cad5fbfbb
Joel Stockamp <stockampj@gmail.com>
2019-08-18 20:29:36 -0700

    Add a number of UI elements, an error message, and will reject results that were appearing in the second and third position even though their return value was less than zero.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
 
   $("#survey-row").fadeIn(2000);
@@ -5,9 +7,9 @@ $(document).ready(function() {
   $("#submit").click(function(event) {
 
     $(".card").removeClass("show");
+    $("#disclaimer").hide();
     $("#survey-row").slideUp(1000);
     $("#results-row").show()
-
 
     var ruby = 0;
     var csharp = 0;
@@ -23,7 +25,10 @@ $(document).ready(function() {
     var q4val = $("#q4-ui").val();
     var q5val = $("#q5-ui").val();
 
-    if (q1val === "2") {
+    if ((q1val === "") && (q2val === "") && (q3val === "") && (q4val === "") && (q5val === "")) {
+      $("#disclaimer").show();
+      return false;
+    } else if (q1val === "2") {
       ruby += 0;
       csharp += 0;
       javascript += 0;
@@ -181,13 +186,13 @@ $(document).ready(function() {
     }
 
 //  tie-breaker
-    ruby += (Math.random()) * .01;
-    csharp += (Math.random()) * .01;
-    javascript += (Math.random()) * .01;
-    rust += (Math.random()) * .01;
-    go += (Math.random()) * .01;
-    python += (Math.random()) * .01;
-    swift += (Math.random()) * .01;
+    ruby -= (Math.random()) * .01;
+    csharp -= (Math.random()) * .01;
+    javascript -= (Math.random()) * .01;
+    rust -= (Math.random()) * .01;
+    go -= (Math.random()) * .01;
+    python -= (Math.random()) * .01;
+    swift -= (Math.random()) * .01;
 
     var languages = [ruby, csharp, javascript, rust, go, python, swift];
     languages.sort(function(a,b){return b-a});
@@ -197,25 +202,25 @@ $(document).ready(function() {
     var third = languages [2];
 
 
-    if ((ruby === first ) || (ruby === second) || (ruby === third)) {
+    if ((ruby === first  && first > 0) || (ruby === second  && second > 0) || (ruby === third && third > 0)) {
       $("#ruby").addClass("show");
     }
-    if ((csharp === first ) || (csharp === second) || (csharp === third)) {
+    if ((csharp === first  && first > 0) || (csharp === second && second > 0) || (csharp === third && third > 0)) {
       $("#csharp").addClass("show");
     }
-    if ((javascript === first ) || (javascript === second) || (javascript === third)) {
+    if ((javascript === first  && first > 0) || (javascript === second && second > 0) || (javascript === third && third > 0)) {
       $("#javascript").addClass("show");
     }
-    if ((go === first ) || (go === second) || (go === third)) {
+    if ((go === first  && first > 0) || (go === second && second > 0) || (go === third && third > 0)) {
       $("#go").addClass("show");
     }
-    if ((python === first ) || (python === second) || (python === third)) {
+    if ((python === first  && first > 0) || (python === second && second > 0) || (python === third && third > 0)) {
       $("#python").addClass("show");
     }
-    if ((rust === first ) || (rust === second) || (rust === third)) {
+    if ((rust === first  && first > 0) || (rust === second && second > 0) || (rust === third && third > 0)) {
       $("#rust").addClass("show");
     }
-    if ((swift === first ) || (swift === second) || (swift === third)) {
+    if ((swift === first  && first > 0) || (swift === second && second > 0) || (swift === third && third > 0)) {
       $("#swift").addClass("show");
     }
 
